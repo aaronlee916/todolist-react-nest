66d16c8c6df2a6c730b55941cca5801a9668a2ad
Aaron Li <aaronlee916@qq.com>
2024-02-20 13:07:35 +0800

    GET() complete

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import axios from "axios";
+import {useState} from 'react'
+
+function App() {
+  const [data, setData] = useState([]);
+  let instance = axios.create({
+    baseURL: "http://localhost:3000",
+    timeout: 60000,
+  });
+  instance.get("/todo-item/get-todo").then(response => setData((response).data)).catch(function(error){
+    console.log(error)
+  });
+
+  return (
+    <>
+      <input />
+      <button>确认</button>
+      <ul>
+        {data.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default App;
