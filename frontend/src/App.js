306dfdaabe5fa9b9063c25439b1f1a8e7a85aa8b
Aaron Li <aaronlee916@qq.com>
2024-02-21 16:11:57 +0800

    some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ let instance = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 60000,
 });
-async function handleButtonClick(data,inputValue,setData) {
+async function handleButtonClick(data, inputValue, setData) {
   await instance
     .post("/post-new-todo-item/new", {
       currArray: data,
@@ -17,12 +17,14 @@ async function handleButtonClick(data,inputValue,setData) {
     });
 }
 
-async function deleteTodoItem(currArray,setData){
-  await instance.delete('/delete-todo-item/delete',{
-    data:{
-      currArray
-    }
-  }).then(res=>setData(res.data))
+async function deleteTodoItem(currArray, setData) {
+  await instance
+    .delete("/delete-todo-item/delete", {
+      data: {
+        currArray,
+      },
+    })
+    .then((res) => setData(res.data));
 }
 
 function App() {
@@ -35,20 +37,38 @@ function App() {
       .catch(function (error) {
         // console.log(error);
       });
-  },[]);
+  }, []);
 
   return (
     <>
-      <input ref={inputValue} />
-      <button onClick={()=>handleButtonClick(data,inputValue.current.value,setData)}>
-        添加
-      </button>
-      <button onClick={()=>deleteTodoItem(data,setData)}>删除</button>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <div className="container">
+        <div className="innerContainer">
+        <div className="controllerContainer">
+        <input ref={inputValue} />
+        <button
+          className="addButton"
+          onClick={() =>
+            handleButtonClick(data, inputValue.current.value, setData)
+          }
+        >
+          添加
+        </button>
+        <button
+          className="deleteButton"
+          onClick={() => deleteTodoItem(data, setData)}
+        >
+          删除
+        </button>
+        </div>
+        <ul className="listContainer">
+          {data.map((item, index) => (
+            <li className="itemContainer" key={index}>
+              {item}
+            </li>
+          ))}
+        </ul>
+        </div>
+      </div>
     </>
   );
 }
